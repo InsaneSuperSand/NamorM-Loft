b98e10c181f5aa95a3fdb125ffd0af148af2619e
InsaneSuperSand <necronomicon93@gmail.com>
2021-10-02 14:50:21 +0200

    dunno

--- js/index.js
+++ js/index.js
@@ -4,3 +4,20 @@ $(function () {
     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
   });
 });
+
+var velocity = 0.5;
+
+function update() {
+  var pos = $(window).scrollTop();
+  $(".container").each(function () {
+    var $element = $(this);
+    // subtract some from the height b/c of the padding
+    var height = $element.height() - 18;
+    $(this).css(
+      "backgroundPosition",
+      "50% " + Math.round((height - pos) * velocity) + "px"
+    );
+  });
+}
+
+$(window).bind("scroll", update);
