c085f26d985d31781afb94cbd0a3a1d00d2f100e
InsaneSuperSand <necronomicon93@gmail.com>
2021-10-03 16:19:26 +0200

    add border on bottom and bootstrap

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
-$(function () {
-  $(document).scroll(function () {
-    var $nav = $(".navbar");
-    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-  });
-});
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if($(window).scrollTop() > $(window).height()){
+            $(".navbar").css({"background-color":"transparent"});   
+        }
+        else{
+            $(".navbar").css({"background-color":"white"});
+        }
+
+    })
+})
