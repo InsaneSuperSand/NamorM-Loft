02600309899cd68574b9836578a9516af27f560e
InsaneSuperSand <necronomicon93@gmail.com>
2021-10-04 19:59:57 +0200

    adding opacity change when scroll, and enlarge second div

--- js/index.js
+++ js/index.js
@@ -1,11 +1,26 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if($(window).scrollTop() > $(window).height()){
-            $(".navbar").css({"background-color":"transparent"});   
-        }
-        else{
-            $(".navbar").css({"background-color":"white"});
-        }
+$(window).scroll(function() {
+  var scrollTop = $(this).scrollTop();
 
-    })
-})
+  $('.mainOverlay').css({
+    opacity: function() {
+      var elementHeight = $(this).height(),
+          opacity = ((1 - (elementHeight - scrollTop) / elementHeight) * 0.8) + 0.2;
+          
+      return opacity;
+    }
+  });
+});
+
+
+$(window).scroll(function() {
+  var scrollTop = $(this).scrollTop();
+
+  $('.smallOverlay').css({
+    opacity: function() {
+      var elementHeight = $(this).height(),
+          opacity = ((1 - (elementHeight - scrollTop) / elementHeight) * 0.8) - 0.2;
+          
+      return opacity;
+    }
+  });
+});
