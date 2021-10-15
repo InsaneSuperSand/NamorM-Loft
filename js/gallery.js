3d412572abd29e982f990a33793f669de076adee
InsaneSuperSand <necronomicon93@gmail.com>
2021-10-15 17:09:20 +0200

    endind about, creating gallery

---
+++ js/gallery.js
@@ -0,0 +1,14 @@
+
+
+$(document).ready(function () {
+  $('.fadeOnLoad').fadeIn('slow');
+});
+
+$(document).ready(function () {
+  $('.fadeOnLoadSmall').fadeIn('slow');
+});
+
+$(window).scroll(function(){
+	$('.navbar').toggleClass('scrolled', $(this).scrollTop() > 50);
+});
+
