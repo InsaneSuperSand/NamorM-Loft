d90d3acf8e869fb9c0c23c599c870182ce123835
InsaneSuperSand <necronomicon93@gmail.com>
2021-10-15 22:29:00 +0200

    creating gallery

--- js/gallery.js
+++ js/gallery.js
@@ -12,3 +12,29 @@ $(window).scroll(function(){
 	$('.navbar').toggleClass('scrolled', $(this).scrollTop() > 50);
 });
 
+
+
+
+// Open the Modal
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
